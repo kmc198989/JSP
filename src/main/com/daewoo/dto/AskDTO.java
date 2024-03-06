20eae266ffc109f118668e37a686019184f4c94f
kmc198989 <kmc198989@naver.com>
2024-03-06 11:21:55 +0900

    전체업로드

---
+++ src/main/com/daewoo/dto/AskDTO.java
@@ -0,0 +1,61 @@
+package main.com.daewoo.dto;
+   
+public class AskDTO {
+     private  String a_post; // 게시글 
+     private  int a_post_num; //게시글 번호 
+     private  String a_post_date; // 게시글 등록 일자 
+     private  String a_comment; //댓글 
+     private  int a_comment_num; //댓글 번호 
+     private  String a_comment_date; //댓글 등록 일자
+     private  String a_check; //문의글 답변 여부
+	public String getA_post() {
+		return a_post;
+	}
+	public void setA_post(String a_post) {
+		this.a_post = a_post;
+	}
+	public int getA_post_num() {
+		return a_post_num;
+	}
+	public void setA_post_num(int a_post_num) {
+		this.a_post_num = a_post_num;
+	}
+	public String getA_post_date() {
+		return a_post_date;
+	}
+	public void setA_post_date(String a_post_date) {
+		this.a_post_date = a_post_date;
+	}
+	public String getA_comment() {
+		return a_comment;
+	}
+	public void setA_comment(String a_comment) {
+		this.a_comment = a_comment;
+	}
+	public int getA_comment_num() {
+		return a_comment_num;
+	}
+	public void setA_comment_num(int a_comment_num) {
+		this.a_comment_num = a_comment_num;
+	}
+	public String getA_comment_date() {
+		return a_comment_date;
+	}
+	public void setA_comment_date(String a_comment_date) {
+		this.a_comment_date = a_comment_date;
+	}
+	public String getA_check() {
+		return a_check;
+	}
+	public void setA_check(String a_check) {
+		this.a_check = a_check;
+	}
+	@Override
+	public String toString() {
+		return "AskDTO [a_post=" + a_post + ", a_post_num=" + a_post_num + ", a_post_date=" + a_post_date
+				+ ", a_comment=" + a_comment + ", a_comment_num=" + a_comment_num + ", a_comment_date=" + a_comment_date
+				+ ", a_check=" + a_check + "]";
+	}
+	
+	
+}
---
+++ src/main/com/daewoo/dto/CommunityDTO.java
@@ -0,0 +1,55 @@
+package main.com.daewoo.dto;
+
+public class CommunityDTO {
+     private String c_post; //게시글 
+     private int c_post_num; // 게시글 번호 
+     private String c_comment;// 댓글 
+     private int c_comment_num;// 댓글 번호 
+     private String c_post_date; //게시글 등록 일자 
+     private String c_comment_date; //댓글 등록 일자 
+   public String getC_post() {
+      return c_post;
+   }
+   public void setC_post(String c_post) {
+      this.c_post = c_post;
+   }
+   public int getC_post_num() {
+      return c_post_num;
+   }
+   public void setC_post_num(int c_post_num) {
+      this.c_post_num = c_post_num;
+   }
+   public String getC_comment() {
+      return c_comment;
+   }
+   public void setC_comment(String c_comment) {
+      this.c_comment = c_comment;
+   }
+   public int getC_comment_num() {
+      return c_comment_num;
+   }
+   public void setC_comment_num(int c_comment_num) {
+      this.c_comment_num = c_comment_num;
+   }
+   public String getC_post_date() {
+      return c_post_date;
+   }
+   public void setC_post_date(String c_post_date) {
+      this.c_post_date = c_post_date;
+   }
+   public String getC_comment_date() {
+      return c_comment_date;
+   }
+   public void setC_comment_date(String c_comment_date) {
+      this.c_comment_date = c_comment_date;
+   }
+   @Override
+   public String toString() {
+      return "CommunityDTO [c_post=" + c_post + ", c_post_num=" + c_post_num + ", c_comment=" + c_comment
+            + ", c_comment_num=" + c_comment_num + ", c_post_date=" + c_post_date + ", c_comment_date="
+            + c_comment_date + "]";
+   }
+     
+     
+   
+}
